6f82f503867944854df3b9d7ca11b7c7e12e15c6
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-06-05 21:17:39 +0530

    Create config.py

---
+++ D3vil/config.py
@@ -0,0 +1,41 @@
+# D3vil- 
+# Copyright (C) 2021  D3KRISH
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Inukaasith
+
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/608bc4d077145abc527be.jpg")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "d3vil_music_player")
+PROJECT_NAME = getenv("PROJECT_NAME", "D3vmusic")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/D3KRISH/D3VILMUSIC-BOT")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
